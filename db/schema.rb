98255703e70acf2e4538caa08865c193f1ddc66a
Eman A. Hmdan <emanadnan109@gmail.com>
2017-11-15 08:52:36 -0800

    added price field, add product, edit in style new and edit page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113095712) do
+ActiveRecord::Schema.define(version: 20171115154904) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,7 +18,8 @@ ActiveRecord::Schema.define(version: 20171113095712) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.string   "colour"
+    t.decimal  "price"
   end
 
 end
