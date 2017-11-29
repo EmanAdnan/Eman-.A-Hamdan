5674c5f4744bac5c06289efce1e32181e840f3e4
Eman A. Hmdan <emanadnan109@gmail.com>
2017-11-29 02:00:32 -0800

    send message

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,5 +1,5 @@
 class UserMailerPreview < ActionMailer::Preview
   def contact_form
-    UserMailer.contact_form("emanadnan109@gmail.com", "Eman", "Hello World!")
+    UserMailer.contact_form("emanadnan279@gmail.com", "Eman", "Hello World!")
   end
 end
