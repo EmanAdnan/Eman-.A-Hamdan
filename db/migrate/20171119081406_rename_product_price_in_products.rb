b6f6f91abe9df9d75edd76680f32e33f8e51b866
Eman A. Hmdan <emanadnan109@gmail.com>
2017-12-08 09:19:14 -0800

    login and signup

--- db/migrate/20171119081406_rename_product_price_in_products.rb
+++ db/migrate/20171119081406_rename_product_price_in_products.rb
@@ -1,5 +1,5 @@
 class RenameProductPriceInProducts < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :products, :product_price, :price
-  end
+	  def change
+	  	# rename_column :products, :product_price, :price
+	  end
 end
