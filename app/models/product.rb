e73680149a0d96c8ffecaed5807e47fb0af86c9e
Eman A. Hmdan <emanadnan109@gmail.com>
2017-12-10 03:49:02 -0800

    comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comments
 
 	def self.search(search_term)
     	search_term.strip! #Returns a copy of str with leading and trailing whitespace removed.
@@ -9,5 +10,14 @@ class Product < ApplicationRecord
       		Product.where('name ilike ?', "%#{search_term}%")
     	end
 	end
+
+def highest_rating_comment
+  comments.rating_desc.first
+end
+
+ def lowest_rating_comment
+    comments.rating_asc.first
+  end
+
 
 end
