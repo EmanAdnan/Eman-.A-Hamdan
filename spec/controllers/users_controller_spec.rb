36c768abe45ef302bcaa0d1068578476a311edf8
Eman A. Hmdan <emanadnan109@gmail.com>
2018-01-06 12:07:05 -0800

    Test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,9 @@
-require 'rails_helper'
+ require 'rails_helper'
 
 describe UsersController do
-before do
+  before do
     @user = FactoryBot.create(:user)
+    @user2 = FactoryBot.create(:user)
   end
 
   describe 'GET #show' do
@@ -10,13 +11,16 @@ before do
       before do
         sign_in @user
       end
-
       it 'loads correct user details' do
         get :show, params: { id: @user.id }
-        expect(response).to be_ok
         expect(assigns(:user)).to eq @user
+        expect(response).to have_http_status(200)
       end
-
+      it 'cant access other users show page' do
+        get :show, params: { id: @user2.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
     end
 
     context 'when a user is not logged in' do
@@ -25,6 +29,6 @@ before do
         expect(response).to redirect_to(new_user_session_path)
       end
     end
-  end
 
+  end
 end
