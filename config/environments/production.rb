f881168cf9c925239ad507ecb098f0cbdb32b5b8
Eman A. Hmdan <emanadnan109@gmail.com>
2018-01-08 02:57:33 -0800

    Cash

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,12 +88,13 @@ end
 
 
 # Memecachier - Dalli
-config.cache_store = :dalli_store
-(ENV["MEMCACHIER_SERVERS"] || "").split(","),
-{:username => ENV["MEMCACHIER_USERNAME"],
-  :password => ENV["MEMCACHIER_PASSWORD"],
-  :failover => true,
-  :socket_timeout => 1.5,
-  :socket_failure_delay => 0.2,
-  :down_retry_delay => 60
-} 
+  require 'dalli'
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :down_retry_delay => 60
+                    }
